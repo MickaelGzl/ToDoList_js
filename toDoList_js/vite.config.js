8cf13927ae00825608d8f476fbb6fa5741dcfe93
Mickael Gonzales <mickael.gonzales@hotmail.com>
2023-02-09 22:53:40 +0100

    ajout icone .svg et .png 144 et 512 pour pwa

--- toDoList_js/vite.config.js
+++ toDoList_js/vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 export default defineConfig({
   plugins: [
-    VitePWA({ registerType: 'autoUpdate' })
+    VitePWA({ registerType: 'autoUpdate' },{includeAssets: ['public/assets/Picto_APP_512.png', 'public/assets/Picto_APP_144.png']})
   ]
 })
